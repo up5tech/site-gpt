fd80b7e3d29c6c4cf2436364be1a204748cee009
Jacky <tdhungit@gmail.com>
2026-08-04 14:52:17 +0700

    Add endpoint to create default settings

--- src/site_gpt/app/api/setting.py
+++ src/site_gpt/app/api/setting.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from site_gpt.app import models
-from site_gpt.app.db.session import get_db
 from site_gpt.app.core.auth import get_admin_user
+from site_gpt.app.db.session import get_db
 from site_gpt.app.schemas.setting import SettingRes
 
 router = APIRouter()
@@ -29,4 +29,25 @@ def update_settings(
             {"value": setting.value}
         )
     db.commit()
-    return
+    return {"message": "Settings updated successfully"}
+
+
+@router.post("/")
+def create_settings(
+    db: Session = Depends(get_db),
+    user=Depends(get_admin_user),
+):
+    default_settings = [
+        models.Setting(
+            key="assistant_name", value="Site GPT", company_id=user.company_id
+        ),
+        models.Setting(
+            key="widget_header_color", value="green", company_id=user.company_id
+        ),
+        models.Setting(
+            key="widget_footer_color", value="green", company_id=user.company_id
+        ),
+    ]
+    db.add_all(default_settings)
+    db.commit()
+    return {"message": "Settings created successfully"}
